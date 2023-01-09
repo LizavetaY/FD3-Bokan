410c8aa78fc0ed386455dca33b53d80830b090ba
Lizaveta <liz07bokan@gmail.com>
2023-01-09 22:57:00 +0100

    feat: add hw Br2JSX

---
+++ br2jsx/App.js
@@ -0,0 +1,8 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+import { BR2JSX } from "./components/BR2JSX/BR2JSX";
+
+const textData = "первый<br>второй<br/>третий<br />последний";
+
+ReactDOM.render(<BR2JSX text={textData} />, document.getElementById("root"));
